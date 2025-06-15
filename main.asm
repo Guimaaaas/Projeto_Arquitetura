41eed1608499594e88c19a8e1325ecd04cecd503
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2025-06-15 20:19:31 -0300

    updates

--- main.asm
+++ main.asm
@@ -33,8 +33,8 @@
 	comando_limpar_dados:			.asciiz "formatar"
 	
 	# Repositorios
-	repositorio_apto:           		.space 4500 # Espaco reservado para a gravacao temporaria dos apartamentos cadastrados
-	apto.txt:     	 			.asciiz  "apartamento.txt"	
+	repositorio_apto:           		.space 7000 # Espaco reservado para a gravacao temporaria dos apartamentos cadastrados
+	apto_repositorio_externo:     	 	.asciiz  "apartamento.txt"	
 	
 	# Mensagens de erro:
 	msg_comando_invalido:			.asciiz "Comando invalido"
@@ -49,4 +49,70 @@
 	msg_apto_vazio:				.asciiz "Apartamento vazio"
 	
 .text
+
+carregar_externo:
+	jal ler_dados          # função que chama syscall e carrega o arquivo
+	j main       	       # segue execução principal
+
+
+main:
+
+	# Ponteiro do buffer de input
+	la $t0, input_usuario      # $t0 = endereço base do buffer
+	li $t1, 0                  # $t1 = offset atual
+
+leitura_input:
+	# Imprimir o banner
+	li $v0, 4
+	la $a0, banner
+	syscall
+
+esperar_tecla:
+	lw $t2, keyboard_status
+	andi $t2, $t2, 1           # Verifica se bit 0 está setado (tecla pressionada)
+	beqz $t2, esperar_tecla    # Se não, espera
+
+	# Ler caractere
+	lw $t3, keyboard_buffer    # $t3 = caractere lido
+
+	sb $t3, 0($t0)             # Salva caractere no buffer
+	addiu $t0, $t0, 1          # Avança ponteiro
+	addi  $t1, $t1, 1          # Conta caractere lido
+
+	# Verifica se é \n (barra_n tem valor 10)
+	la $t4, barra_n
+	lb $t4, 0($t4)
+	beq $t3, $t4, final_input
+
+	j esperar_tecla            # Continua lendo
+
+final_input:
+	li $t5, 0                  # Byte nulo
+	sb $t5, 0($t0)             # Coloca nulo ao final do buffer
+
+	# Aqui você pode seguir com o parser ou comando interpretador
+	j main                     # Por enquanto, reinicia leitura
+
+ler_dados:
+    li   $v0, 13
+    la   $a0, apto_repositorio_externo
+    li   $a1, 0
+    li   $a2, 0
+    syscall
+    move $s0, $v0
+    bltz $s0, fim_carregar
+
+    li   $v0, 14
+    move $a0, $s0
+    la   $a1, repositorio_apto
+    li   $a2, 4500
+    syscall
+
+    li   $v0, 16
+    move $a0, $s0
+    syscall
+
+fim_carregar:
+    jr $ra
+
 
